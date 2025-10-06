2d24b19ab4e3b8a504d143411122a060475d6c7e
Sai Gowtham Talluri <saigowtham.t1299@gmail.com>
2025-10-06 11:13:38 -0500

    added GTM

---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* ✅ Google Analytics 4 (GA4) Global Site Tag */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-WD7CJXHTY1"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WD7CJXHTY1', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
