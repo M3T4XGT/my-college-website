64f3463689f97ed84b248b42cfa0c795ad64ac97
Sai Gowtham Talluri <saigowtham.t1299@gmail.com>
2025-10-06 11:27:51 -0500

    added lib

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
 import "../styles/globals.css";
+import Script from "next/script";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
+
 import Header from "../components/Header";
 import MainNav from "../components/MainNav";
 import Footer from "../components/Footer";
@@ -13,8 +18,41 @@ import DeanQuoteSection from "../components/DeanQuoteSection";
 import HeroSection from "../components/HeroSection";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      {/* ✅ Google Analytics Script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
+      {/* ✅ Your Site Components */}
       <Header />
       <MainNav />
       <HeroSection />
